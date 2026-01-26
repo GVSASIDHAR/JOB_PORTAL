d23f56009af8c3404b8c8e73ffd5f50e21a80a19
GVSASIDHAR <sasidharguturi123@gmail.com>
2026-01-26 16:19:53 +0530

    Changed DB

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -83,13 +83,8 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "127.0.0.1"),  # force TCP (no socket weirdness)
-        "PORT": os.getenv("DB_PORT", "3306"),
-        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 AUTH_USER_MODEL = "accounts.User"
